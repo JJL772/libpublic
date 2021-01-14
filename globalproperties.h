80f8603c62240eb9e33f8453948c67163cf16954
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-01-14 01:06:35 -0800

    feat: Stupid globalproperties

---
+++ globalproperties.h
@@ -0,0 +1,19 @@
+/*
+globalproperty.cpp - Global properties
+Copyright (C) 2020 Jeremy Lorelli
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+#pragma once
+
+/* These are defined as function such that conditionally compiled code can be avoided */
+/* This way, we can ship one set of binaries for the game & dedicated server */
+bool IsDedicatedServer();
